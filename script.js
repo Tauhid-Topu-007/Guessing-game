eddc59fb22c3eecb05a9a38b752f1b568081ae08
tauhid-topu <t.topu021@gmail.com>
2024-07-10 21:01:40 +0600

    console

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ for(var i=1;i<=5;i++){
     var guess= parseInt(prompt("Guess a number between 1 to 5"));
     var randomNumber= Math.floor(Math.random()*5)+1;
     if(randomNumber==guess){
-        document.write("You have won"+"<br>");
+        console.log("You have won"+"<br>");
         numberOfWon++;
     }
     else{
-        document.write("You have lost.The guessing number was ="+ randomNumber +"<br>");
+        console.log("You have lost.The guessing number was ="+ randomNumber +"<br>");
         numberOfLost++;
     }
 }
